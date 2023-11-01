8922bab2d4257581dbe7913f0a9a4e8f9cca3e6b
Mateusz Klocek <psymk6@nottingham.ac.uk>
2023-11-01 10:25:32 +0000

    Working on simulator 2.

--- simulator1.c
+++ simulator1.c
@@ -1,6 +1,32 @@
-#include <courseworkFilesForStudents/source/linkedlist.h>
-#include <courseworkFilesForStudents/source/coursework.h>
+#include "linkedlist.c"
+#include "coursework.c"
+#include <stdio.h>
 
-int main(){
-    printf("Hello World!");
+
+int main() {
+    Process *process;
+    LinkedList *ready;
+    struct timeval start, end;
+
+    //start timer.
+    gettimeofday(&start, NULL);
+    process = generateProcess(0);
+
+    printf("GENERATOR - CREATED: [PID = %d, Priority = %d, InitialBurstTime = %d, RemainingBurstTime = %d]\n",
+           process->iPID, process->iPriority, process->iBurstTime, process->iRemainingBurstTime);
+
+    while (process->iState != TERMINATED) {
+
+        runPreemptiveProcess(process, true);
+        printf("SIMULATOR - [PID = %d, Priority = %d, InitialBurstTime = %d, RemainingBurstTime = %d]\n",
+               process->iPID, process->iPriority, process->iBurstTime, process->iRemainingBurstTime);
+
+    }
+    //end timer for turnaround time.
+    gettimeofday(&end, NULL);
+
+    printf("TERMINATOR - TERMINATED: [PID = %d, ResponseTime = %d, TurnAroundTime = %ld]\n",
+           process->iPID, 0, getDifferenceInMilliSeconds(start, end));
+
+    destroyProcess(process);
 }
